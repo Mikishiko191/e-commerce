7bf383972fae7e6bf655ed27d82cff360e7e7e27
mikishiko191 <mishiko191@gmail.com>
2025-01-13 10:51:51 +0400

    UI library add

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from '@/components/shared/header/UserButton';
+import { Button } from '@/components/ui/button';
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <UserButton />
       main page go here
+      <Button>Button from shancd/UI</Button>
     </main>
   );
 }
